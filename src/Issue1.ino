139af9229a14448fb87bfeebe9ea5e8c0ced087c
Jan-Insllica <jmazaandujar@insllica.cat>
2026-06-09 10:16:16 +0200

    Add files via upload
    closes #7

---
+++ src/Issue1.ino
@@ -0,0 +1,10 @@
+#include <dummy.h>
+
+void setup() {
+  Serial.begin(115200);       // Inicia el port sèrie USB a 115200 baud
+  delay(2000);
+  Serial.println("Hola USB!"); // Escriu un missatge al monitor sèrie
+}
+
+void loop() {
+}
